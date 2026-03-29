490452848e59625ddc113e4d2ac47362efe6f667
Mah <66678444+mahvalenterj@users.noreply.github.com>
2026-03-29 12:51:51 -0300

    Delete src/data directory

--- src/data/stats.js
+++
@@ -1,6 +0,0 @@
-export const stats = [
-  { num: '+1.300', suffix: '+', label: 'Prompts profissionais' },
-  { num: '13', suffix: '', label: 'Nichos cobertos' },
-  { num: '7', suffix: 'dias', label: 'Garantia incondicional' },
-  { num: 'R$97', suffix: '', label: 'Pagamento único' },
-];
